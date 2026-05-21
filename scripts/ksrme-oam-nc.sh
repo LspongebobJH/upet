d81b2407a667f50c7e08db62d9a873780c290a9d
LspongebobJH <lspongebobjh@gmail.com>
2026-05-21 11:59:02 +0800

    update ksrme script to adapt for submit or local execution

--- scripts/ksrme-oam-nc.sh
+++ scripts/ksrme-oam-nc.sh
@@ -6,9 +6,7 @@ export LD_LIBRARY_PATH=/usr/local/nvidia/lib64:$LD_LIBRARY_PATH
 export PATH=/usr/local/nvidia/bin:$PATH
 
 cd /mnt/shared-storage-gpfs2/lijiahang1/jobs/upet
-export PATH="/mnt/shared-storage-user/lijiahang/miniconda3/bin:$PATH"
-. /mnt/shared-storage-user/lijiahang/miniconda3/etc/profile.d/conda.sh
-conda activate pet
+export PATH="/mnt/shared-storage-user/lijiahang/miniconda3/envs/pet/bin:$PATH"
 
 # --------------- Running arguments ---------------
 save_dir="./logs/ksrme/pet-oam-xl-v1.0.0-nc"
@@ -17,8 +15,11 @@ ckpt_path="./checkpoints/pet-oam-xl-v1.0.0.ckpt"
 # --------------- Distributed Setting ---------------
 
 NPROC_PER_NODE=8
-NODE_RANK=${NODE_RANK}
+NODE_COUNT=${NODE_COUNT:-1}
+NODE_RANK=${NODE_RANK:-0}
 export WORLD_SIZE=$(( NPROC_PER_NODE * NODE_COUNT ))
+
+# --------------- Running ---------------
 
 for (( i=0; i<NPROC_PER_NODE; i++ )); do
     export LOCAL_RANK=${i}
@@ -27,7 +28,7 @@ for (( i=0; i<NPROC_PER_NODE; i++ )); do
         --ckpt_path ${ckpt_path} \
         --save_dir ${save_dir} \
         --non_conservative \
-        --distributed
+        --distributed &
 
     # echo RANK:${RANK}, LOCAL_RANK:${LOCAL_RANK}, WORLD_SIZE:${WORLD_SIZE}
 done
